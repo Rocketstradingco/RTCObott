1a0a5f17f7fe8ead519514e73bdf1cc29b1b50e2
Rocketstradingco <rocketstradingco@gmail.com>
2025-07-08 23:26:29 -0500

    Add basic Discord bot and admin web UI

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, redirect, render_template, render_template_string, session
+from data_manager import load_data, save_data
+from dotenv import load_dotenv
+import os
+
+load_dotenv('example.env')
+
+app = Flask(__name__)
+app.secret_key = os.getenv('ADMIN_PASSWORD', 'change-me')
+
+
+def require_login(func):
+    def wrapper(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect('/')
+        return func(*args, **kwargs)
+    wrapper.__name__ = func.__name__
+    return wrapper
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('password') == os.getenv('ADMIN_PASSWORD'):
+            session['logged_in'] = True
+            return redirect('/inventory')
+    return render_template('index.html')
+
+
+@app.route('/inventory')
+@require_login
+def inventory():
+    data = load_data()
+    return render_template('inventory.html', data=data)
+
+
+@app.route('/add-category', methods=['GET', 'POST'])
+@require_login
+def add_category():
+    data = load_data()
+    if request.method == 'POST':
+        name = request.form.get('name')
+        cat_id = str(len(data['categories']) + 1)
+        data['categories'].append({'id': cat_id, 'name': name, 'cards': []})
+        save_data(data)
+        return redirect('/inventory')
+    return render_template_string('''\
+        {% extends 'layout.html' %}
+        {% block content %}
+        <h2>Add Category</h2>
+        <form method="post">
+            <label>Name: <input type="text" name="name"></label>
+            <button type="submit" class="button">Save</button>
+        </form>
+        {% endblock %}
+    ''')
+
+
+@app.route('/category/<cat_id>', methods=['GET', 'POST'])
+@require_login
+def manage_category(cat_id):
+    data = load_data()
+    cat = next((c for c in data['categories'] if c['id'] == cat_id), None)
+    if not cat:
+        return 'Category not found', 404
+    if request.method == 'POST' and request.form.get('action') == 'add-card':
+        card = {
+            'id': str(len(cat['cards']) + 1),
+            'name': request.form.get('name'),
+            'front': request.form.get('front'),
+            'back': request.form.get('back'),
+            'claimed_by': None
+        }
+        cat['cards'].append(card)
+        save_data(data)
+    return render_template('category.html', category=cat)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ bot.py
@@ -0,0 +1,151 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+from data_manager import load_data, save_data
+
+load_dotenv('example.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+def build_category_embed(cat):
+    embed = discord.Embed(title=cat['name'], description='Explore cards')
+    return embed
+
+class ExploreView(discord.ui.View):
+    def __init__(self, user, cat):
+        super().__init__(timeout=300)
+        self.user = user
+        self.cat = cat
+        self.index = 0
+        self.per_page = 9
+        self.update_children()
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        return interaction.user.id == self.user.id
+
+    def update_children(self):
+        self.clear_items()
+        cards = self.cat['cards'][self.index:self.index + self.per_page]
+        rows = 3
+        for i, card in enumerate(cards):
+            button = discord.ui.Button(label=card['name'], style=discord.ButtonStyle.grey, row=i // rows)
+            button.callback = self.make_view_card(card)
+            self.add_item(button)
+        if self.index > 0:
+            prev_btn = discord.ui.Button(label='Prev', style=discord.ButtonStyle.blurple)
+            prev_btn.callback = self.prev_page
+            self.add_item(prev_btn)
+        if self.index + self.per_page < len(self.cat['cards']):
+            next_btn = discord.ui.Button(label='Next', style=discord.ButtonStyle.blurple)
+            next_btn.callback = self.next_page
+            self.add_item(next_btn)
+
+    def make_view_card(self, card):
+        async def callback(interaction: discord.Interaction):
+            embed = discord.Embed(title=card['name'])
+            embed.set_image(url=card['front'])
+            view = CardView(self.user, self.cat, card)
+            await interaction.response.edit_message(embed=embed, view=view)
+        return callback
+
+    async def prev_page(self, interaction: discord.Interaction):
+        self.index = max(0, self.index - self.per_page)
+        self.update_children()
+        await interaction.response.edit_message(view=self)
+
+    async def next_page(self, interaction: discord.Interaction):
+        self.index += self.per_page
+        self.update_children()
+        await interaction.response.edit_message(view=self)
+
+class CardView(discord.ui.View):
+    def __init__(self, user, cat, card):
+        super().__init__(timeout=300)
+        self.user = user
+        self.cat = cat
+        self.card = card
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        return interaction.user.id == self.user.id
+
+    @discord.ui.button(label='Left', style=discord.ButtonStyle.secondary)
+    async def left(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed = discord.Embed(title=self.card['name'])
+        embed.set_image(url=self.card['back'])
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @discord.ui.button(label='Right', style=discord.ButtonStyle.secondary)
+    async def right(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed = discord.Embed(title=self.card['name'])
+        embed.set_image(url=self.card['front'])
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @discord.ui.button(label='Claim', style=discord.ButtonStyle.green)
+    async def claim(self, interaction: discord.Interaction, button: discord.ui.Button):
+        data = load_data()
+        cat = next((c for c in data['categories'] if c['id'] == self.cat['id']), None)
+        card = next((x for x in cat['cards'] if x['id'] == self.card['id']), None)
+        if card and not card.get('claimed_by'):
+            card['claimed_by'] = interaction.user.name
+            save_data(data)
+            await interaction.response.send_message('Claimed!', ephemeral=True)
+        else:
+            await interaction.response.send_message('Already claimed', ephemeral=True)
+
+    @discord.ui.button(label='Unclaim', style=discord.ButtonStyle.red)
+    async def unclaim(self, interaction: discord.Interaction, button: discord.ui.Button):
+        data = load_data()
+        cat = next((c for c in data['categories'] if c['id'] == self.cat['id']), None)
+        card = next((x for x in cat['cards'] if x['id'] == self.card['id']), None)
+        if card and card.get('claimed_by') == interaction.user.name:
+            card['claimed_by'] = None
+            save_data(data)
+            await interaction.response.send_message('Unclaimed', ephemeral=True)
+        else:
+            await interaction.response.send_message('Cannot unclaim', ephemeral=True)
+
+    @discord.ui.button(label='Back', style=discord.ButtonStyle.secondary)
+    async def back(self, interaction: discord.Interaction, button: discord.ui.Button):
+        view = ExploreView(self.user, self.cat)
+        embed = build_category_embed(self.cat)
+        await interaction.response.edit_message(embed=embed, view=view)
+
+@bot.command()
+async def register(ctx):
+    data = load_data()
+    for cat in data['categories']:
+        embed = build_category_embed(cat)
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(label='Explore', custom_id=f'explore_{cat["id"]}'))
+        msg = await ctx.send(embed=embed, view=view)
+        cat['message_id'] = msg.id
+    save_data(data)
+    await ctx.send('Registration complete.')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.event
+async def on_interaction(interaction: discord.Interaction):
+    if interaction.type == discord.InteractionType.component:
+        custom = interaction.data.get('custom_id', '')
+        if custom.startswith('explore_'):
+            cat_id = custom.split('_', 1)[1]
+            data = load_data()
+            cat = next((c for c in data['categories'] if c['id'] == cat_id), None)
+            if not cat:
+                await interaction.response.send_message('Category missing', ephemeral=True)
+                return
+            view = ExploreView(interaction.user, cat)
+            embed = build_category_embed(cat)
+            await interaction.response.send_message(embed=embed, ephemeral=True, view=view)
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
